6c0f3811724683e96eb44608d155569fcc8943cb
龍弥 山田 <r.yamada@datakit.co.jp>
2023-11-24 18:23:36 +0900

    モデル追加

---
+++ app/CSMS/CSMS/Models/ContractModel.cs
@@ -0,0 +1,20 @@
+﻿using CSMS.Models.ValueObject;
+using MessagePack;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CSMS.Models
+{
+    public class ContractModel
+    {
+        [Key]
+        public Guid ContractId { get; private set; }
+        [Required, NotNull, StringLength(50)]
+        public string ContractName { get; private set; }
+        [Required, NotNull, StringLength(50)]
+        public string ContractCode { get; private set; }
+        public Guid? CustomerId { get; private set; }
+        public IMoneyModel Money { get; private set; }
+
+    }
+}
---
+++ app/CSMS/CSMS/Models/ValueObject/MoneyModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+
+namespace CSMS.Models.ValueObject
+{
+    public interface IMoneyModel
+    {
+        public decimal CalcTaxMoney(decimal money, decimal tax);
+    }
+    public class MoneyModel : IMoneyModel
+    {
+        private decimal Money { get; }
+        private decimal TaxMoney { get; }
+
+        public MoneyModel(decimal money) 
+        {
+            this.Money = money;
+        }
+        public decimal CalcTaxMoney (decimal money, decimal tax)
+        {
+            MoneyModel _money = new MoneyModel(money);
+            return _money.Money * tax;
+        }
+    }
+}
