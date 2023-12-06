520ccf0765243e8262811aa73e2a88d56de412ab
龍弥 山田 <r.yamada@datakit.co.jp>
2023-12-06 18:14:45 +0900

    delete money from ContractDto

--- app/CSMS/CSMS/DTO/Contract/ContractDto.cs
+++ app/CSMS/CSMS/DTO/Contract/ContractDto.cs
@@ -6,7 +6,6 @@ public class ContractDto
     public string ContractName { get; set; }
     public string ContractCode { get; set; }
     public Guid CustomerId { get; set; }
-    public decimal Money { get; set; }
     public decimal AmountExcludingTax { get; set; }
     public decimal TaxRate { get; set; }
 }
