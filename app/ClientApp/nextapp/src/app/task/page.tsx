dc78e9d25014d3f4e45d073951714458d4dd9105
Nahaq <naha022677@icloud.com>
2023-12-29 03:23:01 +0900

    crate task ui list

--- app/ClientApp/nextapp/src/app/task/page.tsx
+++ app/ClientApp/nextapp/src/app/task/page.tsx
@@ -8,6 +8,7 @@ import useSWR from "swr";
 import styles from "./task.module.css";
 import { Box } from "@mui/material";
 import { ContrastOutlined } from "@mui/icons-material";
+import { red } from "@mui/material/colors";
 
 interface Task {
   taskId: string;
@@ -32,21 +33,63 @@ const Task: React.FC<TaskProps> = (): React.JSX.Element => {
   const fetcher = <T,>(url: string): Promise<T> =>
     axios.get(url).then((res: AxiosResponse<T>) => res.data);
   const { data, error } = useSWR<Task[]>("/api/Task/", fetcher);
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.main}>
         <div className={styles.container}>
-          <div className={styles.text}>Task</div>
+          <h2 className={styles.title}>Task</h2>
           <ul>
-            <li>
-              {data &&
-                data.map((item) => (
-                  <Box key={item.taskId}>
-                    {item.taskName}
-                    {item.contents}
-                  </Box>
-                ))}
-            </li>
+            <Box sx={{ padding: "20px", display: "flex" }}>
+              <Box>
+                <Box
+                  sx={{
+                    margin: "10px 0 20px 20px",
+                    gap: "20px",
+                  }}
+                >
+                  <span className={styles.column}>Name</span>
+                </Box>
+                {data &&
+                  data.map((item) => (
+                    <Box
+                      key={item.taskId}
+                      sx={{
+                        padding: "10px 20px 20px 20px",
+                        display: "flex",
+                        gap: "20px",
+                        fontSize: "1.5em",
+                      }}
+                    >
+                      <Box>{item.taskName}</Box>
+                    </Box>
+                  ))}
+              </Box>
+              <Box>
+                <Box
+                  sx={{
+                    margin: "10px 0 20px 20px",
+                    gap: "20px",
+                  }}
+                >
+                  <span className={styles.column}>Contents</span>
+                </Box>
+                {data &&
+                  data.map((item) => (
+                    <Box
+                      key={item.taskId}
+                      sx={{
+                        padding: "10px 20px 20px 20px",
+                        display: "flex",
+                        gap: "20px",
+                        fontSize: "1.5em",
+                      }}
+                    >
+                      <Box>{item.contents}</Box>
+                    </Box>
+                  ))}
+              </Box>
+            </Box>
           </ul>
         </div>
       </div>
