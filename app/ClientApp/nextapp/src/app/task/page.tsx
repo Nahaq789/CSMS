81dce56a155d09e254b481981295b9ab599f269e
Nahaq <naha022677@icloud.com>
2023-12-29 18:00:20 +0900

    add task list use by mui/datagrid

--- app/ClientApp/nextapp/src/app/task/page.tsx
+++ app/ClientApp/nextapp/src/app/task/page.tsx
@@ -6,9 +6,22 @@ import { promises } from "dns";
 import React from "react";
 import useSWR from "swr";
 import styles from "./task.module.css";
-import { Box } from "@mui/material";
-import { ContrastOutlined } from "@mui/icons-material";
-import { red } from "@mui/material/colors";
+import { Box, LinearProgress } from "@mui/material";
+import { ContrastOutlined, Key } from "@mui/icons-material";
+import { blue, blueGrey, green, grey, red } from "@mui/material/colors";
+import {
+  DataGrid,
+  GridToolbar,
+  GridColDef,
+  GridRowsProp,
+  GridRowModesModel,
+  GridEventListener,
+  GridRowEditStopReasons,
+  GridRowId,
+  GridRowModes,
+  GridRowModel,
+} from "@mui/x-data-grid";
+import { idID } from "@mui/material/locale";
 
 interface Task {
   taskId: string;
@@ -22,74 +35,164 @@ interface TaskProps {
   task: Task[];
 }
 
-interface Card {
-  id: string;
-  name: string;
-  content: string;
-  any: undefined;
+interface EdirTollbarProps {
+  setRows: (newRows: (oldRows: GridRowsProp) => GridRowsProp) => void;
+  setRowModesModel: (
+    newModel: (oldModel: GridRowsProp) => GridRowsProp
+  ) => void;
+}
+
+function EditToolbar(props: EdirTollbarProps) {
+  const { setRows, setRowModesModel } = props;
+
+  // const handleClick = () => {
+  // const id = rows.find;
+  // setRows((oldRows) => [...oldRows, { id, name: '', age: '', isNew: true }]);
+  // setRowModesModel((oldModel) => ({
+  //   ...oldModel,
+  //   [id]: { mode: GridRowModes.Edit, fieldToFocus: 'name' },
+  // }));
 }
 
 const Task: React.FC<TaskProps> = (): React.JSX.Element => {
   const fetcher = <T,>(url: string): Promise<T> =>
     axios.get(url).then((res: AxiosResponse<T>) => res.data);
-  const { data, error } = useSWR<Task[]>("/api/Task/", fetcher);
+  const { data, error } = useSWR<GridRowsProp<Task>>("/api/Task/", fetcher);
+  const [rows, setRows] = React.useState(data);
+  const [rowModesModel, setRowModesModel] = React.useState<GridRowModesModel>(
+    {}
+  );
+
+  const handleRowEditStop: GridEventListener<"rowEditStop"> = (
+    params,
+    event
+  ) => {
+    if (params.reason === GridRowEditStopReasons.rowFocusOut) {
+      event.defaultMuiPrevented = true;
+    }
+  };
+
+  const handleEditClick = (id: GridRowId) => () => {
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
+  };
+
+  const handleSaveClick = (id: GridRowId) => () => {
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+  };
+
+  const handleDeleteClick = (id: GridRowId) => () => {
+    setRows(rows?.filter((row) => row.taskId !== id));
+  };
+
+  const handleCancelClick = (id: GridRowId) => () => {
+    setRowModesModel({
+      ...rowModesModel,
+      [id]: { mode: GridRowModes.View, ignoreModifications: true },
+    });
+
+    const editedRow = rows?.find((row) => row.taskId === id);
+    setRows(rows?.filter((row) => row.taskId !== id));
+  };
+
+  const processRowUpdate = (newRow: GridRowModel) => {
+    const UpdateRow: Task = {
+      ...newRow,
+      taskId: "",
+      taskName: "",
+      contents: "",
+      customerId: "",
+      contractId: "",
+    };
+    setRows(
+      rows?.map((row) => (row.taskId === newRow.taskId ? UpdateRow : row))
+    );
+    return UpdateRow;
+  };
+
+  const handleRowModesModelChange = (newRowModesModel: GridRowModesModel) => {
+    setRowModesModel(newRowModesModel);
+  };
+
+  const columns: GridColDef[] = [
+    { field: "taskId", headerName: "ID", flex: 1 },
+    {
+      field: "taskName",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "contents",
+      headerName: "Content",
+      flex: 1,
+    },
+    {
+      field: "customerId",
+      headerName: "CustomerID",
+      flex: 1,
+    },
+    {
+      field: "contractId",
+      headerName: "ContractID",
+      flex: 1,
+    },
+  ];
 
   return (
     <div className={styles.wrapper}>
       <div className={styles.main}>
         <div className={styles.container}>
-          <h2 className={styles.title}>Task</h2>
-          <ul>
-            <Box sx={{ padding: "20px", display: "flex" }}>
-              <Box>
-                <Box
-                  sx={{
-                    margin: "10px 0 20px 20px",
-                    gap: "20px",
-                  }}
-                >
-                  <span className={styles.column}>Name</span>
-                </Box>
-                {data &&
-                  data.map((item) => (
-                    <Box
-                      key={item.taskId}
-                      sx={{
-                        padding: "10px 20px 20px 20px",
-                        gap: "20px",
-                        fontSize: "1.5em",
-                      }}
-                    >
-                      <Box>{item.taskName}</Box>
-                    </Box>
-                  ))}
-              </Box>
-              <Box>
-                <Box
-                  sx={{
-                    margin: "10px 0 20px 20px",
-                    gap: "20px",
-                  }}
-                >
-                  <span className={styles.column}>Contents</span>
-                </Box>
-                {data &&
-                  data.map((item) => (
-                    <Box
-                      key={item.taskId}
-                      sx={{
-                        padding: "10px 20px 20px 20px",
-                        display: "flex",
-                        gap: "20px",
-                        fontSize: "1.5em",
-                      }}
-                    >
-                      <Box>{item.contents}</Box>
-                    </Box>
-                  ))}
-              </Box>
+          <Box>
+            <h2 className={styles.title}>Task</h2>
+            <Box
+              m="40px 0 0 0"
+              height="70vh"
+              sx={{
+                "& .MuiDataGrid-root": {
+                  border: "none",
+                },
+                "& .MuiDataGrid-cell": {
+                  borderBottom: "none",
+                  color: "#b7bac1",
+                },
+                "& .name-column--cell": {
+                  color: green[400],
+                },
+                "& .MuiDataGrid-columnHeaders": {
+                  backgroundColor: blueGrey[200],
+                  borderBottom: "none",
+                  // color: "#b7bac1",
+                  borderRadius: "10px",
+                },
+                "& .MuiDataGrid-virtualScroller": {
+                  // backgroundColor: "red",
+                },
+                "& .MuiDataGrid-footerContainer": {
+                  borderTop: "none",
+                  backgroundColor: blueGrey[200],
+                  // color: "#b7bac1",
+                  borderRadius: "10px",
+                },
+                "& .MuiCheckbox-root": {
+                  color: green,
+                },
+                "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+                  color: "#b7bac1",
+                },
+              }}
+            >
+              <DataGrid
+                rows={data || []}
+                columns={columns}
+                slots={{
+                  toolbar: GridToolbar,
+                }}
+                getRowId={(row) => row.taskId}
+                onRowClick={console.log}
+                disableRowSelectionOnClick
+              />
             </Box>
-          </ul>
+          </Box>
         </div>
       </div>
     </div>
